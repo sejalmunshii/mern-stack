09b2102d7144ccdb33039d7370f1e180a653413b
sejalmunshii <sejalmunshi11@gmail.com>
2026-01-23 17:23:11 +0530

    add

--- node/01_events.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter =require("events");
-
-const emitter=new EventEmitter();
-
-emitter.on("greet",()=>{
-    console.log("hello");
-})
-
-emitter.emit("greet");
--- node/index.js
+++
@@ -1,9 +0,0 @@
-let http=require("http");
-
-http.createServer((req,res)=>{
-    res.end("heelo seejall");
-    console.log("url",req.url)
-    console.log("method",req.method)
-    console.log("header",req.headers)
-}).listen(5000,()=> console.log("server started"))
-
